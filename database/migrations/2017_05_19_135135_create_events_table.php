64d85259cdd294332982976ab888f816c3ba9a7e
saraElskely <sara.5star@yahoo.com>
2017-05-19 16:59:14 +0200

    database models

---
+++ database/migrations/2017_05_19_135135_create_events_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->dateTime('event_date');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->integer('subcategory_id')->unsigned();
+            $table->foreign('subcategory_id')
+                ->references('id')
+                ->on('subcategories')
+                ->onDelete('cascade');
+
+            $table->string('address');
+            $table->string('latitude');
+            $table->string('longitude');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
