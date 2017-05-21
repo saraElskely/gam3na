c5afba833086671be403145ffe1b55ddbbf30e3d
esraa <Esraaelmasry696@yahoo.com>
2017-05-21 18:33:49 +0200

    admin CRUD user subcategory

--- database/migrations/2017_05_19_135313_create_reviews_table.php
+++ database/migrations/2017_05_19_135313_create_reviews_table.php
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
-
-class CreateReviewsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->increments('id');
-            $table->text('content');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
-
-                $table->integer('event_id')->unsigned();
-                $table->foreign('event_id')
-                    ->references('id')
-                    ->on('events')
-                    ->onDelete('cascade');
-
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('reviews');
-    }
-}
-||||||| merged common ancestors
-=======
 <?php
 
 use Illuminate\Support\Facades\Schema;
@@ -89,4 +42,4 @@ class CreateReviewsTable extends Migration
         Schema::dropIfExists('reviews');
     }
 }
->>>>>>> 173c0b320438bc032a59d502daf3609833601959
+
