45b292d311f3b40028d42f775c29361aad5075c4
saraElskely <sara.5star@yahoo.com>
2017-06-06 20:26:32 +0200

    after event date + tagmi3

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -108,7 +108,7 @@
     </section>
 
 
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" >
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
